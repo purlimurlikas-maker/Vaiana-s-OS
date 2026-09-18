d28ca67e7deee84e4e0136e2d580172c8f1bb729
purlimurlikas-maker <purlimurlikas@gmail.com>
2026-09-18 21:56:33 +0300

    getting some things to work

--- script.js
+++ script.js
@@ -1,43 +1,42 @@
-['welcome'].forEach(id => {
-  const el = document.getElementById(id);
-  if (el) dragElement(el);
-});
+const welcome = document.getElementById("welcome");
+const handle = document.getElementById("welcomeheader");
 
-function dragElement(el) {
-  let startX = 0, startY = 0, prevX = 0, prevY = 0;
-  if (!el) return;
+let dragState = null;
 
-  el.onmousedown = dragMouseDown;
-  function dragMouseDown(e) {
-    e = e || window.event;
-    e.preventDefault();
-    prevX = e.clientX;
-    prevY = e.clientY;
-    document.onmouseup = closeDragElement;
-    document.onmousemove = elementDrag;
-  }
-
-  function elementDrag(e) {
-    e = e || window.event;
-    e.preventDefault();
-    startX = prevX - e.clientX;
-    startY = prevY - e.clientY;
-    prevX = e.clientX;
-    prevY = e.clientY;
-    el.style.top = (el.offsetTop - startY) + "px";
-    el.style.left = (el.offsetLeft - startX) + "px";
-  }
-
-  function closeDragElement() {
-    document.onmouseup = null;
-    document.onmousemove = null;
-  }
+if (handle) {
+  handle.addEventListener("mousedown", startDrag);
 }
 
-if (document.getElementById(elmnt.id + "header")) {
-	// if present, the header is where you move the DIV from:
-	document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
-} else {
-	// otherwise, move the DIV from anywhere inside the DIV:
-	elmnt.onmousedown = dragMouseDown;
+function startDrag(e) {
+  if (!welcome) return;
+
+  const rect = welcome.getBoundingClientRect();
+
+  dragState = {
+    startX: e.clientX,
+    startY: e.clientY,
+    left: rect.left,
+    top: rect.top
+  };
+
+  welcome.style.transform = "none";
+  document.addEventListener("mousemove", onDrag);
+  document.addEventListener("mouseup", stopDrag);
+  e.preventDefault();
+}
+
+function onDrag(e) {
+  if (!dragState) return;
+
+  const dx = e.clientX - dragState.startX;
+  const dy = e.clientY - dragState.startY;
+
+  welcome.style.left = `${dragState.left + dx}px`;
+  welcome.style.top = `${dragState.top + dy}px`;
+}
+
+function stopDrag() {
+  document.removeEventListener("mousemove", onDrag);
+  document.removeEventListener("mouseup", stopDrag);
+  dragState = null;
 }
